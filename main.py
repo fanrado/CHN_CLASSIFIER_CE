97e405194cc13c9f14206f90776cb29d9f95a3a3
fanrado <radofana@gmail.com>
2025-06-04 12:01:32 -0700

    changes testing the change in BDT.py

--- main.py
+++ main.py
@@ -9,7 +9,16 @@ if __name__=='__main__':
     # split_train_test_dataset(path_to_data=root_path, frac_test=0.2) 20% of the whole dataset is used for testing
 
     #
-    classifier = BDT_Classifier()
-    # classifier.read_data(path_to_data='data/labelledData/train_valid')
-    params = classifier.tune_hyperamaters()
-    classifier.train(params=params)
+    # # CLASSIFICATION MODEL
+    # path_to_data = 'data/labelledData'
+    # output_path = 'OUTPUT/bdt'
+    # classifier = BDT_Classifier(path_to_data=path_to_data, output_path=output_path)
+    # params = classifier.tune_hyperamaters()
+    # classifier.train(params=params)
+
+    #
+    # REGRESSION MODEL
+    regressor = BDT_Regressor(path_to_data='data/labelledData', output_path='OUTPUT/bdt')
+    params = regressor.tune_hyperparameters()
+    regressor.train(params=params)
+    regressor.classify_predicted_integralMaxdev(path_to_classifier_model='OUTPUT/bdt/classifier_bdt_model.json')
