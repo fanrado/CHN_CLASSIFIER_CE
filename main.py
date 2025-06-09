1b5a3b50e77a7bf540c95819e929e258df86416f
fanrado <radofana@gmail.com>
2025-06-08 18:53:41 -0700

    minor change: testing

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from BDT import split_train_test_dataset, BDT_Classifier, BDT_Regressor
 
 if __name__=='__main__':
     # train_test split
-    # root_path = 'data/labelledData'
-    # split_train_test_dataset(path_to_data=root_path, frac_test=0.2) 20% of the whole dataset is used for testing
+    # root_path = 'data/labelledData/labelledData'
+    # split_train_test_dataset(path_to_data=root_path, frac_test=0.2) #20% of the whole dataset is used for testing
 
     #
     # CLASSIFICATION MODEL
-    path_to_data = 'data/labelledData/fitparams_data'
-    output_path = 'OUTPUT/bdt'
-    classifier = BDT_Classifier(path_to_data=path_to_data, output_path=output_path)
-    params = classifier.tune_hyperamaters()
-    classifier.train(params=params)
+    # # path_to_data = 'data/labelledData/fitparams_data'
+    # path_to_data = 'data/labelledData/labelledData'
+    # output_path = 'OUTPUT/bdt'
+    # classifier = BDT_Classifier(path_to_data=path_to_data, output_path=output_path)
+    # params = classifier.tune_hyperamaters()
+    # classifier.train(params=params)
 
     #
     # REGRESSION MODEL
-    regressor = BDT_Regressor(path_to_data='data/labelledData/fitparams_data', output_path='OUTPUT/bdt')
+    path_to_data = 'data/labelledData/labelledData'
+    regressor = BDT_Regressor(path_to_data=path_to_data, output_path='OUTPUT/bdt')
     params = regressor.tune_hyperparameters()
     regressor.train(params=params)
     regressor.classify_predicted_integralMaxdev(path_to_classifier_model='OUTPUT/bdt/classifier_bdt_model.json')
