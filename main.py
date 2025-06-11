785d084070a0296ca96a8947bdc1692ef0e4857f
fanrado <radofana@gmail.com>
2025-06-11 15:44:16 -0700

    added the test of the new class Classifier

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, sys
 import pandas as pd
 import numpy as np
-from BDT import split_train_test_dataset, BDT_Classifier, BDT_Regressor
+from BDT import split_train_test_dataset, BDT_Classifier, BDT_Regressor, Classifier
 
 if __name__=='__main__':
     # train_test split
@@ -10,17 +10,28 @@ if __name__=='__main__':
 
     #
     # CLASSIFICATION MODEL
-    # # path_to_data = 'data/labelledData/fitparams_data'
-    # path_to_data = 'data/labelledData/labelledData'
-    # output_path = 'OUTPUT/bdt'
-    # classifier = BDT_Classifier(path_to_data=path_to_data, output_path=output_path)
-    # params = classifier.tune_hyperamaters()
-    # classifier.train(params=params)
+    # path_to_data = 'data/labelledData/fitparams_data'
+    path_to_data = 'data/labelledData/labelledData'
+    path_to_data = 'data/labelledData/labelledData_gpuSamples_alot/'
+    output_path = 'OUTPUT/bdt'
+    classifier = BDT_Classifier(path_to_data=path_to_data, output_path=output_path)
+    params = classifier.tune_hyperamaters()
+    classifier.train(params=params)
 
     #
     # REGRESSION MODEL
-    path_to_data = 'data/labelledData/labelledData'
+    # path_to_data = 'data/labelledData/labelledData'
     regressor = BDT_Regressor(path_to_data=path_to_data, output_path='OUTPUT/bdt')
     params = regressor.tune_hyperparameters()
     regressor.train(params=params)
     regressor.classify_predicted_integralMaxdev(path_to_classifier_model='OUTPUT/bdt/classifier_bdt_model.json')
+
+    # # CLASSIFICATION OF THE FIT PARAMETERS : DATA
+    combined_classifier = Classifier(regressor_model='OUTPUT/bdt/regressor_bdt_model.json',
+                                     classifier_model='OUTPUT/bdt/classifier_bdt_model.json',
+                                     path_to_data='data/labelledData',
+                                     output_path='OUTPUT/bdt/TestOnData')
+    combined_classifier.run_classification(info_data_dict={'key_in_name': 'fit_results',
+                                                           'file_ext': '.csv',
+                                                           'sep': ','},
+                                            plot2dcorr=True, plotconfmatrix=True)
