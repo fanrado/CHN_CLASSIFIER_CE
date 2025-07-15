df607b7b7ec91a4d55f0068373812a06aff1cff7
fanrado <radofana@gmail.com>
2025-07-15 12:05:12 -0400

    send the realresponse with the corresponding time axis to the front end for a comparison with the calculated waveform

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import CSVModel
+from app.models import CSVModel, ROOTmodel
 import pandas as pd
 import numpy as np
 from flask_cors import CORS
@@ -9,7 +9,8 @@ CORS(api, resources={r"/data/*": {"origins": "http://localhost:3000"}})
 # CORS(api)
 # Initialize the CSVModel
 # csv_model = CSVModel(csv_file_path='data/fit_results_run_30360_no_avg_labelled_tails.csv', model_file_path='xgboost_model.json')
-csv_model = CSVModel(csv_file_path='data/fit_results_run_30360_no_avg_labelled_tails.csv', regressor_model_file_path='models/regressor_bdt_model.json', classifier_model_file_path='models/classifier_bdt_model.json')
+csv_model   = CSVModel(csv_file_path='data/fit_results_run_30360_no_avg_labelled_tails.csv', regressor_model_file_path='models/regressor_bdt_model.json', classifier_model_file_path='models/classifier_bdt_model.json')
+root_model  = ROOTmodel(root_file_path='data/raw_waveforms_run_30413.root', hist_prefix='hist_0') # read the root data
 
 @api.route('/health', methods=['GET'])
 def health_check():
@@ -25,17 +26,23 @@ def get_data(data_id):
                  'c3': 'overshoot with small undershoot',
                  'c4': 'overshoot only'}
     try:
-        record = csv_model.data[csv_model.data['#Ch.#']==int(data_id)]
+        chn     = int(data_id)
+        # record  = csv_model.data[csv_model.data['#Ch.#']==int(data_id)]
+        record  = csv_model.data[csv_model.data['#Ch.#']==chn]
         # print(record) #
         if record.empty:
             return jsonify({"error": "Record not found"}), 404
-        intR_maxdev, pred_class = csv_model.predict(record)
-        idx = record.index[0]
-        record.loc[idx, 'integral_R'] = np.round(intR_maxdev.iloc[0]['integral_R'], 4)
+        intR_maxdev, pred_class         = csv_model.predict(record)
+        idx                             = record.index[0]
+        record.loc[idx, 'integral_R']   = np.round(intR_maxdev.iloc[0]['integral_R'], 4)
         record.loc[idx, 'max_deviation'] = np.round(intR_maxdev.iloc[0]['max_deviation'], 4)
-        record.loc[idx, 'class'] = class_map[pred_class[0]]
-        
-        return jsonify(record.to_dict(orient='records')[0]), 200
+        record.loc[idx, 'class']        = class_map[pred_class[0]]
+        record_dict                     = record.to_dict(orient='records')[0]
+        tticks, wf                      = root_model.getCHN_resp(chn=chn)
+        record_dict['realResp']         = wf
+        record_dict['timeticks']        = tticks
+        # return jsonify(record.to_dict(orient='records')[0]), 200
+        return jsonify(record_dict), 200
         # return json_record, 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
